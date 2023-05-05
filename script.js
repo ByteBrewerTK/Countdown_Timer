a4a39497edb59ce00ecb53697d4b787fa199ad92
Abutalif Khan <105064795+ByteBrewerTK@users.noreply.github.com>
2023-05-05 18:15:04 +0530

    Countdown Timer webpage final JS_PracticeChallenge 1 (No responsiveness)

---
+++ script.js
@@ -0,0 +1,28 @@
+// Countdown Project
+
+const endDate = "06 May 2023 08:00 AM";
+document.getElementById("end-date").innerText = endDate;
+
+const input = document.querySelectorAll("input");
+
+const clock = () => {
+    const end = new Date(endDate);
+    const now = new Date();
+
+    const diff = (end-now)/1000;
+
+    // For Days
+    input[0].value = Math.floor(diff/3600/24);
+
+    // Hours
+    input[1].value = Math.floor(diff/3600)%24;
+
+    // Minutes
+    input[2].value = Math.floor(diff/60)%60;
+
+    // Seconds
+    input[3].value = Math.floor(diff%60);
+   
+}
+setInterval(clock, 1000);
+
